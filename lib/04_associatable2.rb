5d2d0761475c9e772e1b2483f3ab3e1011c67905
Michael <michaelowenpark@gmail.com>
2016-03-13 22:43:03 -0700

    remove comments

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,12 +1,10 @@
 require_relative '03_associatable'
 
-# Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
     define_method(name) do
-      result = self.send(through_name).send(source_name)
+       = self.send(through_name).send(source_name)
       if result
         return result
       else
